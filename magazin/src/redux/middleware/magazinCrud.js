7cdae64bd1c964463320543bfd367199569d42aa
rachelsin <rmpodo@gmail.com>
2021-06-24 10:40:48 +0300

    start project

--- magazin/src/redux/middleware/magazinCrud.js
+++ magazin/src/redux/middleware/magazinCrud.js
@@ -1,7 +1,7 @@
 import { actions } from "../actions/action";
 
-export const creatMagazin=({dispatch,getState})=>next=>action=>{
-    if(action.type==='CREAT_MAGAZIN'){
+export const creatMagazin = ({ dispatch, getState }) => next => action => {
+    if (action.type === 'CREAT_MAGAZIN') {
         alert("creat magazin")
     }
     return next(action);
